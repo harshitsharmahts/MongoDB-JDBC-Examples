f3e3810f0b0e76125a86a039637302e5c4d71ccc
Harshit Sharma <harshitsharmahts@gmail.com>
2018-01-05 09:04:00 +0530

    connection

---
+++ src/MongoConnection.java
@@ -0,0 +1,22 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoConnection
+{
+	private static final String HOST = "127.0.0.1";
+	private static final int PORT = 27017;
+
+	public static void main(String[] args)
+	{
+		try {
+			MongoClient mongo = new MongoClient( HOST , PORT );
+
+			MongoDatabase database = mongo.getDatabase("foobar");
+
+			System.out.println("Successfully connected..");
+		}
+		catch (Exception e) {
+			System.err.println( e.getClass().getName() + ": " +e.getMessage() );
+		}
+	}
+}
